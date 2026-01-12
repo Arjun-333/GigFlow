844a596ddeb931815aadb104cb6d0054b324c44f
Arjun-333 <arjunr252005@gmail.com>
2026-01-12 21:04:03 +0530

    Added ScrollToTop behavior and cleaned up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import { AuthProvider } from './context/AuthContext';
 
 // Pages (Placeholders for now)
@@ -16,15 +17,15 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="min-h-screen bg-gray-50">
+        <ScrollToTop />
+        <div className="min-h-screen bg-gray-50 flex flex-col">
           <Navbar />
-          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex-grow w-full">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/gigs" element={<GigsList />} />
-              <Route path="/gigs/create" element={<CreateGig />} />
               <Route path="/gigs/create" element={<CreateGig />} />
               <Route path="/gigs/:id" element={<GigDetails />} />
               <Route path="*" element={<NotFound />} />
